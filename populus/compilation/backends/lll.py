1089fa439b9958c8242fa55aaeff170470ddd9e4
Noel Maersk <veox@users.noreply.github.com>
2017-12-08 18:00:15 +0200

    backends/lll: try loading .lll.abi file + WIP compiler interfacing.

--- populus/compilation/backends/lll.py
+++ populus/compilation/backends/lll.py
@@ -5,26 +5,31 @@ from .base import (
 
 class LLLBackend(BaseCompilerBackend):
     project_source_glob = ('*.lll')
-    test_source_glob = ('test_*.lll.py')
+    test_source_glob = ('test_*.lll')
 
     def get_compiled_contracts(self, source_file_paths, import_remappings):
         try:
-            raise Exception # TODO: check for binary
+            from ethereum_lll import compiler # TODO: implement
         except ImportError:
             raise ImportError(
-                'lllc needs to be installed to use LLLBackend as compiler backend.'
+                'Package ethereum-lll needs to be installed to use LLLBackend as compiler backend.'
             )
 
         self.logger.debug("Compiler Settings: %s", pprint.pformat(self.compiler_settings))
 
         compiled_contracts = []
 
-        # TODO: boilerplate from ViperBackend - move up?
         for contract_path in source_file_paths:
             code = open(contract_path).read()
-            abi = compiler.mk_full_signature(code)
+            try:
+                abi = open(contract_path + '.abi').read()
+            except FileNotFoundError as e:
+                self.logger.error(".lll files require an accompanying .lll.abi JSON ABI file!")
+                raise e
+
             bytecode = '0x' + compiler.compile(code).hex()
             bytecode_runtime = '0x' + compiler.compile(code, bytecode_runtime=True).hex()
+
             compiled_contracts.append({
                 'name': os.path.basename(contract_path).split('.')[0],
                 'abi': abi,
